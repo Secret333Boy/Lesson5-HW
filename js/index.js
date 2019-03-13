000c5c0b8d8e248c6e54c7a43ac2b647bb6254d2
Evgeniy <eegensky@gmail.com>
2019-03-13 21:19:15 +0200

    Lesson 5 HW (2)

--- js/index.js
+++ js/index.js
@@ -2,28 +2,40 @@
 // Условный оператор не использовать.
 
 /*1*/
-var N = 5, nums = [],sign = [], numsWithSigns = [], result = 0;
+// var N = 5, nums = [],sign = [], numsWithSigns = [], result = 0;
 
-for (var i = 0; i < N; i++) {
-	nums.push(i+1);
-}
+// for (var i = 0; i < N; i++) {
+// 	nums.push(i+1);
+// }
 
-console.log(`Nums: ` + nums);
+// console.log(`Nums: ` + nums);
 
-for (var i = 0; i < N; i+=2) {
-	sign.push("+");
-	sign.push("-");
-}
+// for (var i = 0; i < N; i+=2) {
+// 	sign.push("+");
+// 	sign.push("-");
+// }
 
-console.log(`Signs: ` + sign);
+// console.log(`Signs: ` + sign);
 
-for (var i = 0; i < nums.length; i++) {
-	numsWithSigns.push(+`${sign[i]}${nums[i]}`);
-}
-var task = "(" + numsWithSigns.join(')+(') + ")";
-console.log(`Task: ` + task);
+// for (var i = 0; i < nums.length; i++) {
+// 	numsWithSigns.push(+`${sign[i]}${nums[i]}`);
+// }
+// var task = "(" + numsWithSigns.join(')+(') + ")";
+// console.log(`Task: ` + task);
 
-for (var i = 0; i < numsWithSigns.length; i++) {
-	result += numsWithSigns[i];
-}
-console.log(result)
+// for (var i = 0; i < numsWithSigns.length; i++) {
+// 	result += numsWithSigns[i];
+// }
+// console.log(result);
+
+/*2*/
+// var N = 6, result = 0, j = 1;
+
+// for (var i = 1; i <= N; i++) {
+// 	console.log(`${result} + ${i*j}:`);
+// 	result += i * j;
+// 	console.log(`${result}`);
+// 	j *= -1;
+// }
+
+// console.log(`Result: ${result}`);
